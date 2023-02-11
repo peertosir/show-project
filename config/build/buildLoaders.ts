0db8db679ce158f3b78f42e35c0e662cd47a50fd
peertosir <peertosir@gmail.com>
2023-02-12 01:24:27 +0300

    Update gitignore

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,33 @@
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import webpack from "webpack";
+import { BuildOptions } from "./types/config";
+
+export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+    const typescriptLoader = {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node-modules/,
+    }
+
+    const cssLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            {
+                loader: "css-loader",
+                options: {
+                    modules: {
+                        auto: /\.module\.s[ac]ss$/i,
+                        localIdentName: isDev ? '[path][name]__[local]--[hash:base64:5]' : '[hash:base64:8]'
+                    }
+                }
+            },
+            "sass-loader"
+        ]
+    }
+
+    return [
+        typescriptLoader,
+        cssLoader
+    ]
+}
